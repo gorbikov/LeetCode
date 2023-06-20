d087b2e3b7b87575ab9223ac45338957d0665977
Denis Gorbikov <denis@gorbikov.ru>
2023-06-20 14:51:16 +0300

    Вторая задачка работает, но недостаточно быстро.

--- 12. Integer to Roman.py
+++ 12. Integer to Roman.py
@@ -4,12 +4,23 @@ class Solution(object):
         :type num: int
         :rtype: str
         """
+        roman_dict = {"M": 1000, "CM": 900, "D": 500, "CD": 400, "C": 100, "XC": 90, "L": 50, "XL": 40, "X": 10,
+                      "IX": 9, "V": 5, "IV": 4, "I": 1}
+        result = []
+
+        for key in roman_dict.keys():
+            value = roman_dict[key]
+            while num >= value:
+                result.append(key)
+                num -= roman_dict[key]
+
+        return "".join(result)
 
 
 s = Solution()
 print("Example 1: ")
-print(s.romanToInt(3))
+print(s.intToRoman(3))
 print("Example 2: ")
-print(s.romanToInt(58))
+print(s.intToRoman(58))
 print("Example 3: ")
-print(s.romanToInt(1994))
+print(s.intToRoman(1994))
--- 13. Roman to Integer.py
+++ 13. Roman to Integer.py
@@ -6,7 +6,6 @@ class Solution(object):
         """
         letters = list(s)
         result = 0
-        to_pop = []
         dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000, "IV": 4,
                 "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
         for count, value in enumerate(letters):
