d38a773dde0d8951034b85893d35ea9588874f96
Denis Gorbikov <denis@gorbikov.ru>
2023-07-12 12:01:59 +0300

    Сделал 1514, но очень неэффективно.

--- 1514. Path with Maximum Probability.py
+++ 1514. Path with Maximum Probability.py
@@ -1,20 +1,63 @@
+import itertools
+
+
 class Solution:
     def maxProbability(self, n: int, edges: list[list[int]], succProb: list[float], start: int, end: int) -> float:
-        current_max_succ_prob = 0
 
-        def path_finder(n: int, edges: list[list[int]], succProb: list[float], start: int, end: int):
-            for el in edges:
-                if current_point in el:
-                    current_point_index =
-                    if !(el[!el.index(current_point)] in passed_points):
+        def find_all_paths(n, start, end):
+            """Строит все возможные ребра, через которые можно пройти между start и end."""
+            all_points = list(range(n))
+            all_points.remove(start)
+            all_points.remove(end)
+            all_points = all_points + ['empty'] * len(all_points)
+            all_permutations = itertools.permutations(all_points, r=int(len(all_points) / 2))
+            all_paths = []
+            for i in all_permutations:
+                i = list(i)
+                while 'empty' in i: i.remove('empty')
+                all_paths.append(tuple([start] + i + [end]))
 
-            def path_finder(n: int, edges: list[list[int]], succProb: list[float], start: int, end: int)
+            return (list(set(all_paths)))
 
+        def delete_impossible_paths(all_paths, edges):
+            """Оставляет только возможные пути с учётом edges."""
+            possible_paths = []
+            for current_path in all_paths:
+                possible = True
+                for i in range(len(current_path) - 1):
+                    window = [current_path[i], current_path[i + 1]]
+                    reversed_window = [current_path[i + 1], current_path[i]]
+                    if not (window in edges) and not (reversed_window in edges):
+                        possible = False
+                        break
+                if possible:
+                    possible_paths.append(current_path)
 
+            return possible_paths
 
+        def calculate_max_prob(possible_paths, edges, succProb):
+            max_prob = 0
+            for current_path in possible_paths:
+                current_prob = 1
+                for i in range(len(current_path) - 1):
+                    window = [current_path[i], current_path[i + 1]]
+                    reversed_window = [current_path[i + 1], current_path[i]]
+                    if window in edges:
+                        index = edges.index(window)
+                        current_prob *= succProb[index]
+                    else:
+                        index = edges.index(reversed_window)
+                        current_prob *= succProb[index]
+                if current_prob > max_prob:
+                    max_prob = current_prob
+            return max_prob
 
-        return current_max_succ_prob
-
+        all_paths = find_all_paths(n, start, end)
+        possible_paths = delete_impossible_paths(all_paths, edges)
+        if not possible_paths:
+            return 0
+        else:
+            return calculate_max_prob(possible_paths, edges, succProb)
 
 
 s = Solution()
@@ -24,3 +67,7 @@ print("Example 2: ")
 print(s.maxProbability(3, [[0, 1], [1, 2], [0, 2]], [0.5, 0.5, 0.3], 0, 2))
 print("Example 3: ")
 print(s.maxProbability(3, [[0, 1]], [0.5], 0, 2))
+print("Example 4: ")
+print(s.maxProbability(5, [[0, 1], [0, 2], [1, 3], [3, 4]], [0.5, 0.3, 0.2, 0.1], 0, 2))
+print("Example 5: ")
+print(s.maxProbability(4, [[0, 1], [1, 2], [2, 3], [3, 4]], [0.5, 0.5, 0.5, 0.5], 0, 3))
