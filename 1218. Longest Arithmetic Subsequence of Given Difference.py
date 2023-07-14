4ab4ca3de0ad024c04807835b204bca87f9e9dd2
Denis Gorbikov <denis@gorbikov.ru>
2023-07-14 13:33:28 +0300

    Работает 1218, но превышение по времени на 34 примере из 39.

---
+++ 1218. Longest Arithmetic Subsequence of Given Difference.py
@@ -0,0 +1,68 @@
+from collections import defaultdict
+from heapq import heappop, heappush
+
+
+class Solution:
+    def longestSubsequence(self, arr: list[int], difference: int) -> int:
+        def create_graph(arr, difference):
+            graph = defaultdict(list)
+            for curr_node, curr_val in enumerate(arr):
+                for next_node, next_val in enumerate(arr):
+                    if next_node <= curr_node:
+                        continue
+                    else:
+                        if curr_val + difference == next_val:
+                            graph[curr_node].append(next_node)
+            return graph
+
+        def find_head_nodes(graph):
+            nodes = []
+            for node in graph:
+                write = True
+                for next_node in graph:
+                    if next_node >= node:
+                        pass
+                    if node in graph[next_node]:
+                        write = False
+                        break
+                if write:
+                    nodes.append(node)
+            return nodes
+
+        def find_longest_path_length(graph, node):
+            min_heap = [(-1, node)]
+            visited = set()
+            result = 0
+
+            while min_heap:
+                length, node = heappop(min_heap)
+                visited.add(node)
+                for neighbor_node in graph[node]:
+                    if neighbor_node not in visited:
+                        heappush(min_heap, (length - 1, neighbor_node))
+                        if result > length - 1:
+                            result = length - 1
+            return -result
+
+        graph = create_graph(arr, difference)
+        nodes = find_head_nodes(graph)
+        if not graph:
+            return 1
+        else:
+            result = 1
+            for node in nodes:
+                current_result = find_longest_path_length(graph, node)
+                if current_result > result:
+                    result = current_result
+            return result
+
+
+s = Solution()
+print("Example 1: ")
+print(s.longestSubsequence([1, 2, 3, 4], 1))  # 4
+print("Example 2: ")
+print(s.longestSubsequence([1, 3, 5, 7], 1))  # 1
+print("Example 3: ")
+print(s.longestSubsequence([1, 5, 7, 8, 5, 3, 4, 2, 1], -2))  # 4
+print("Example 4: ")
+print(s.longestSubsequence([1, 2, 3, 4, 8, 9, 10, 11, 12, 13], 1))  # 6
