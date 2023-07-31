5659145822f8df2a2e88198e0bc61cee89073bfe
Denis Gorbikov <denis@gorbikov.ru>
2023-07-31 20:21:52 +0300

    Закончил проблему 712. Оч красиво получилось.

--- 712. Minimum ASCII Delete Sum for Two Strings.py
+++ 712. Minimum ASCII Delete Sum for Two Strings.py
@@ -26,6 +26,22 @@ class Solution:
 
         return calculate_for_substring(s1 + s2) - calculate_for_substring(max_substring) * 2
 
+class Solution:
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        matrix = [[0 for i in range(len(s1)+2)] for j in range(len(s2) + 2)]
+        for i in range(len(s1)):
+            matrix[0][i + 2] = ord(s1[i])
+        for j in range(len(s2)):
+            matrix[j+2][0] = ord(s2[j])
+        for i in range(len(s1)):
+            for j in range(len(s2)):
+                if s1[i] == s2[j]:
+                    matrix[j+2][i+2] = matrix[j+1][i+1] + ord(s1[i])
+                else:
+                    matrix[j + 2][i + 2] = max(matrix[j + 1][i + 2], matrix[j + 2][i + 1])
+        return sum([ord(i) for i in s1 + s2]) - 2 * matrix[-1][-1]
+
+
 
 s = Solution()
 print("Example 1: ")
