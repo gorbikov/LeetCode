caa5256f40dcab88480f1828bc21f66d19cce39f
Denis Gorbikov <denis@gorbikov.ru>
2023-09-14 14:27:04 +0300

    Решаю 332, пока не выходит.

---
+++ 332. Reconstruct Itinerary_in_progress.py
@@ -0,0 +1,39 @@
+from collections import defaultdict
+
+
+class Solution:
+    def findItinerary(self, tickets: list[list[str]]) -> list[str]:
+        path_length = len(tickets)
+        graph = defaultdict(list[str])
+        for ticket in sorted(tickets):
+            graph[ticket[0]].append(ticket[1])
+
+        # Проверка на пустой массив.
+        if path_length == 0:
+            return []
+
+        def find_path(prev_path, graph, remaining_length):
+            if remaining_length == 0:
+                return prev_path
+            else:
+                current_node = prev_path[-1]
+                current_next_nodes = graph[current_node]
+                for next_node in current_next_nodes.copy():
+                    prev_path.append(next_node)
+                    remaining_length -= 1
+                    graph[current_node].remove(next_node)
+                    return find_path(prev_path, graph, remaining_length)
+                    remaining_length += 1
+                    graph[current_node].append(next_node)
+                    prev_path.pop()
+
+        result = find_path(["JFK"], graph, path_length)
+        return result
+
+
+s = Solution()
+# print("Example 1:", s.findItinerary(
+#     [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]))  # ["JFK","MUC","LHR","SFO","SJC"]
+# print("Example 2:", s.findItinerary([["JFK", "SFO"], ["JFK", "ATL"], ["SFO", "ATL"], ["ATL", "JFK"],
+#                                      ["ATL", "SFO"]]))  # ["JFK","ATL","JFK","SFO","ATL","SFO"]
+print("Example 3:", s.findItinerary([["JFK", "KUL"], ["JFK", "NRT"], ["NRT", "JFK"]]))  # ["JFK","NRT","JFK","KUL"]
